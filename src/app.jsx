ede6f33633d753d7d496ee50e4d17efcae9215fe
Kim Gunnarsson <kim@morgondag.nu>
2016-06-21 22:50:39 +0200

    initial build

---
+++ src/app.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+export default class extends React.Component {
+	constructor(props) {
+		super(props)
+	}
+	componentDidMount(){
+
+	}
+	render(){
+		return(
+			<article>
+				<h1>imprint-x</h1>
+			</article>
+		)
+	}
+}
