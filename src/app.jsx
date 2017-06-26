604a48211cb0686edc82eee2d331001af391d243
Kim Aarnseth <kim@morgondag.nu>
2017-06-26 20:10:51 +0200

    x

--- src/app.jsx
+++ src/app.jsx
@@ -85,7 +85,6 @@ export default class extends React.Component {
             let j = Math.floor(Math.random() * i);
             [a[i - 1], a[j]] = [a[j], a[i - 1]];
         }
-
         return a
     }
 
